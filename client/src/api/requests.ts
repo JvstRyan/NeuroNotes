c105079a0a12798df2b0ef740f7dd795c86ab77a
Ryan Horrmann <ryanhorrmann123@gmail.com>
2024-01-11 12:04:02 +0100

    Replacing useEffect with React Query for fetching and creating tasks

--- client/src/api/requests.ts
+++ client/src/api/requests.ts
@@ -1,11 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-interface TaskData {
+export interface TaskData {
     _id: string;
     name: string;
   }
 
+//Get all tasks
 const requestTasks = async () => {
     try {
      const response = await axios.get("http://localhost:5000/api/tasks");
@@ -20,3 +21,9 @@ export const useFetchTasks = () =>
     queryKey: ['tasks'],
     queryFn: requestTasks
   })
+
+//Create task
+export const postTask = async (newTask: { name: string }) => {
+    const response = await axios.post("http://localhost:5000/api/tasks", newTask);
+    return response.data.tasks;
+  };
--- client/src/components/tasks/TaskForm.tsx
+++ client/src/components/tasks/TaskForm.tsx
@@ -1,42 +1,52 @@
-import { Button, FormControl, Input, Spacer, border } from "@chakra-ui/react";
-import axios from "axios";
+import { Button, FormControl, Input, Spacer } from "@chakra-ui/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import { FaPlus } from "react-icons/fa6";
+import { postTask } from "../../api/requests";
 
 const TaskForm = () => {
   const [namedTask, setNamedTask] = useState("");
 
-  const createTask = async () => {
-    try {
-      await axios.post("http://localhost:5000/api/tasks", { name: namedTask });
-      setNamedTask('')
-    } catch (error) {
-      console.error(`You cannot post this task ${error}`);
-    }
-  };
+  const queryClient = useQueryClient();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    createTask();
+  const mutation = useMutation({
+    mutationFn: postTask, 
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['tasks']});
+    },
+});
+  
+  const createTask = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutation.mutate({ name: namedTask });
+    setNamedTask('');
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit} style={{ marginTop: "2rem" }}>
+      <form onSubmit={createTask} style={{ marginTop: "2rem" }}>
         <FormControl>
           <Input
             type="text"
             h={"3rem"}
-            variant={'flushed'}
-            focusBorderColor={'black'}
+            variant={"flushed"}
+            focusBorderColor={"black"}
             placeholder="Enter your daily tasks📃"
-            w={{base: '24rem', sm:  '28rem', md: '35rem'}}
+            w={{ base: "24rem", sm: "28rem", md: "35rem" }}
             value={namedTask}
             onChange={(e) => setNamedTask(e.target.value)}
           ></Input>
           <Spacer h={"1rem"} />
-          <Button _active={{transform: 'scale(0.98)', bg: 'white'}} _hover={{bg: 'white'}} variant={'outline'} border={'2px solid black'} w={{base: '24rem', sm:  '28rem', md: '35rem'}} h={"3rem"} type="submit">
-          <FaPlus />
+          <Button
+            _active={{ transform: "scale(0.98)", bg: "white" }}
+            _hover={{ bg: "white" }}
+            variant={"outline"}
+            border={"2px solid black"}
+            w={{ base: "24rem", sm: "28rem", md: "35rem" }}
+            h={"3rem"}
+            type="submit"
+          >
+            <FaPlus />
           </Button>
         </FormControl>
       </form>
