4bb5dac2aa96feffe2f02075464a8ea2b5d77f58
Ryan Horrmann <ryanhorrmann123@gmail.com>
2024-01-19 14:11:05 +0100

    setup react router and starting to implement login / sign up functionality

---
+++ client/src/components/login/LoginForm.tsx
@@ -0,0 +1,22 @@
+import { Box, Image } from '@chakra-ui/react'
+import React from 'react'
+
+const LoginForm = () => {
+  return (
+    <Box position="relative">
+    <video
+      autoPlay
+      loop
+      muted
+     
+    >
+      <source src="/NeuroNotes.mp4" type="video/mp4" />
+    </video>
+    <Box position="relative">
+      LoginForm
+    </Box>
+  </Box>
+  )
+}
+
+export default LoginForm
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,8 @@ import './index.css'
 import { ChakraProvider} from '@chakra-ui/react'
 import {QueryClientProvider, QueryClient} from '@tanstack/react-query'
 import { theme } from './theme.ts'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/router.tsx'
 
 const queryClient = new QueryClient()
 
@@ -15,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
     <ChakraProvider theme={theme}>
-    <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>
