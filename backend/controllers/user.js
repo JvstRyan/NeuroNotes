0c1106499bb9962331894a29260e16064c3da063
Ryan Horrmann <ryanhorrmann123@gmail.com>
2024-01-22 08:30:49 +0100

    routes and server setup implemented for user login/signup

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,2 +1,15 @@
 const expressAsyncHandler = require("express-async-handler");
+const jwt = require('jsonwebtoken')
+const User = require('../models/userModal')
 
+
+const signup = expressAsyncHandler(async(req,res) => {
+    res.send('User has signed up')
+})
+
+const login = expressAsyncHandler(async(req,res) => {
+    res.send('User has logged in')
+})
+
+
+module.exports = {signup, login}
--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ const dotenv = require("dotenv");
 dotenv.config();
 const connectDB = require("./database/connect");
 const port = process.env.PORT || 5000;
-const userRoutes = require("./routes/taskRouter");
+const taskRoutes = require("./routes/taskRouter");
 const goalRoutes = require("./routes/goalRouter");
 const folderRoutes = require("./routes/noteFolderRouter");
 const noteRoutes = require("./routes/noteRouter");
 const quoteRoutes = require("./routes/quoteRouter");
 const bookRoutes = require("./routes/bookRouter");
+const userRoutes = require('./routes/userRouter')
 const cors = require("cors");
 
 connectDB();
@@ -17,21 +18,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/tasks", userRoutes);
+app.use("/api/tasks", taskRoutes);
 app.use("/api/goals", goalRoutes);
 app.use("/api/notes", noteRoutes);
 app.use("/api/folders", folderRoutes);
 app.use("/api/quotes", quoteRoutes);
 app.use("/api/books", bookRoutes);
+app.use('/api/users', userRoutes)
 
 app.get("/", (req, res) => {
   res.status(200).send("Server is running");
 });
 
-//get all tasks    .get
-//get single task  .get
-//create task      .post
-//update task      .patch
-//Delete task      .delete
 
 app.listen(port, () => console.log(`Server has started on port ${port}`));
